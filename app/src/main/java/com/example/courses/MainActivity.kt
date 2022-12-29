c7f733c87bb81a0648478b90c4e8f75050ace53a
jcisking <krisloeffler@gmail.com>
2022-12-28 20:42:35 -0700

    another commit

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.courses
 
+import android.graphics.BlendModeColorFilter
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courses.ui.Datasource
@@ -27,7 +37,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            CourseGrid()
+            CourseGrid(Datasource.courseTopics)
         }
 
     }
@@ -36,18 +46,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CourseTopicCard(modifier: Modifier = Modifier, courseTopic: CourseTopic, @DrawableRes decorativeIcon: Int) {
     Card(modifier = modifier
-        .padding(10.dp)
-        .fillMaxWidth(),
-        elevation = 5.dp,
-        backgroundColor = Color.Green) {
-        Row() {
-            Image(painter = painterResource(id = courseTopic.imageResourceId),
-                contentDescription = null)
-            Column() {
-                Text(text = stringResource(id = courseTopic.courseNameResourceId))
+        .padding(3.dp),
+        elevation = 5.dp
+        ) {
+        Row(modifier = Modifier.fillMaxHeight()) {
+            Image(modifier = Modifier.size(68.dp), painter = painterResource(id = courseTopic.imageResourceId),
+                contentDescription = null,
+            )
+            Column(modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp, bottom = 8.dp)
+                .fillMaxSize()
+            ) {
+                Text(text = stringResource(id = courseTopic.courseNameResourceId),
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    style = MaterialTheme.typography.body2
+                )
                 Row() {
-                    Image(painter = painterResource(id = decorativeIcon), contentDescription = null)
-                    Text(text = courseTopic.numberOfCourses )
+                    Icon(tint = Color.Black, modifier = Modifier.size(15.dp), painter = painterResource(id = decorativeIcon), contentDescription = null)
+                    Text(modifier = Modifier
+                        .padding(start = 8.dp),
+                        text = courseTopic.numberOfCourses,
+                        style = MaterialTheme.typography.caption)
                 }
             }
         }
@@ -57,16 +76,12 @@ fun CourseTopicCard(modifier: Modifier = Modifier, courseTopic: CourseTopic, @Dr
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun CourseGrid() {
+fun CourseGrid(courseTopicList: List<CourseTopic>) {
     LazyVerticalGrid(cells = GridCells.Fixed(2)) {
-        this.items(items = Datasource.courseTopics) { courseTopic ->
+        items(courseTopicList) { courseTopic ->
             CourseTopicCard(courseTopic = courseTopic, decorativeIcon = R.drawable.ic_grain)
-
-
         }
-
     }
-
 }
 
 
